adaafc65a2a67673d399c7aff40db1c927ff243a
Roman Rubinshteyn <romanrubbk96@mail.ru>
2022-03-31 12:28:58 +0300

    modified people and message services

--- services/message.services.js
+++ services/message.services.js
@@ -1,5 +1,15 @@
 const fs = require("fs")
 
+function Message(messageTitle, messageId) {
+    this.messageTitle = messageTitle;
+    this.messageId = messageId;
+}
+
+let message1 = new Message('hello', 1);
+let message2 = new Message('good morning', 2);
+let messages = []
+messages.push(message1, message2);
+
 class MessageServices {
     getMessages() {
         return new Promise((res, rej) => {
@@ -7,6 +17,11 @@ class MessageServices {
                 if (err) throw err;
                 res(JSON.parse(data))
             })
+        })
+    }
+    getMessagesById(id) {
+        return new Promise((res, rej) => {
+            res(messages.find(item => item.messageId == id))
         })
     }
 }
--- services/people.services.js
+++ services/people.services.js
@@ -1,5 +1,15 @@
 const fs = require("fs")
 
+function People(id, name, age) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+}
+let people1 = new People(1, 'Roman', 25)
+let people2 = new People(2, 'Alesya', 23)
+let people = []
+people.push(people1, people2);
+
 class PeopleServices {
     getPeople() {
         return new Promise((res, rej) => {
@@ -7,6 +17,11 @@ class PeopleServices {
                 if (err) throw err;
                 res(JSON.parse(data))
             })
+        })
+    }
+    getPeopleById(id){
+        return new Promise((res, rej) => {
+            res(people.find(item => item.id == id))
         })
     }
 }
